aac2505c56102090faad9aa44b4d9e0a34eeda90
Thomas <thomaslee9474@gmail.com>
2019-02-11 13:35:22 -0800

    [changes]

---
+++ bin/routes/db.js
@@ -0,0 +1,9 @@
+// Scholarship.create(result_obj)
+    //   .then((dbSchol) => {
+    //     console.log(dbSchol); 
+    //   })
+    //   .catch((err) => {
+    //     return res.json(err);
+    //   });
+    // res.send('Scrape complete');
+
--- bin/webscrape.js
+++ bin/webscrape.js
@@ -1,29 +1,12 @@
 /* eslint-disable camelcase */
 /* eslint-disable no-undef */
 /* eslint-disable func-names */
-// MIDDLEWARE IMPORTS TO POPULATE DB WITH SCHOLARSHIPS
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-
-// RUN WEBSCRAPE AS INSTANCE OF EXPRESS
-const webscrape_app = express()
-
-// INITIALISE MONGODB AND CONNECTION
-require('./data/scholarboard-db');
-
-// IMPORT MODELS
-const Scholarship = require('../models/scholarship');
 
 // DATA SCRAPE FROM SCHOLARSHIPS.COM
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: true });
 const cheerio = require('cheerio');
-
-// CONFIG MIDDLEWARE
-webscrape_app.use(logger("dev")); // Use morgan logger for logging requests
-webscrape_app.use(bodyParser.urlencoded({ extended: true })); // Use body-parser for handling form submissions
+const fs = require('fs');
 
 let url = 'https://www.scholarships.com/financial-aid/college-scholarships/scholarships-by-major/accounting-scholarships/%C2%A1adelante-fund-millercoors-colorado-scholarship/'
 // # TODO: ASK DANI IF YOU CAN LOOP THROUGH AN ARRAY WITH .togo WITH NIGHTMARE
@@ -41,38 +24,40 @@ nightmare
     // LOADING HTML
     const $ = cheerio.load(result);
 
-    const result_obj = {}; 
-
     // VARIBALES THAT NEED TO BE ADDED TO THE MODEL
-    const scholName = $('#ulScholNameli.scholname div').text();
+    // const scholName = $('#innercontent nth-child(2)').text();
+    const scholName = $('.eyebrow').next().text();
     const scholDeadline = $('#due-date-text').text(); 
-    const scholFunding = $('award-info-row div').text();
+    const scholFunding = $('.award-info-row :nth-child(1)').text();
     // SCHOLARSHIP CONTACT INFORMATION
     const scholContact1 = $('#liAddress1Text').text(); 
     const scholContact2 = $('#liAddress2Text').text();
-    const scholContact3 = $('liCityStateZIPText').text(); 
+    const scholContact3 = $('#liCityStateZIPText').text(); 
     const scholContact4 = $('#ulScholDetails li:nth-child(8)').text(); 
     const scholContact = scholContact1 + scholContact2 + scholContact3 + scholContact4;
     // VARIBALE THAT WILL NEED TO BE QUIERIED AFTER SAVED TO DB
     const scholRequirements = $('#ulScholDetails li.scholdescrip div').text();
 
     // SAVE TEXT AS PROPERTY OF RESULT OBJ
-    result_obj.name = scholName;
-    result_obj.deadline = scholDeadline;
-    result_obj.funding = scholFunding;
-    result_obj.contactInfo = scholContact;
-    result_obj.requirements = scholRequirements;
+    const result_obj = {
+      name: scholName,
+      deadline: scholDeadline,
+      funding: scholFunding,
+      contactInfo: scholContact,
+      requirements: scholRequirements,
+    };
 
-    Scholarship.create(result_obj)
-      .then((dbSchol) => {
-        console.log(dbSchol); 
-      })
-      .catch((err) => {
-        return res.json(err);
-      });
-    res.send('Scrape complete');
+    // STORE AS JSON
+    const storeData = (data, path) => {
+      try {
+        fs.writeFileSync(path, JSON.stringify(data));
+      } catch (err) {
+        console.error(err)
+      }
+    };
+    // for object in range(1, 1000)
+    storeData(result_obj, `/data/${result_obj.name}.json`);
+  })
+  .catch((err) => {
+    console.log(err)
   });
-
-// START SERVER
-const port = process.env.PORT_WEBSCRAPE;
-webscrape_app.listen(port);
