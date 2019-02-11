f0a230c8b52741e9a1f426b1405b33152282d574
Thomas <thomaslee9474@gmail.com>
2019-02-11 15:58:58 -0800

    [update] model

--- models/scholarship.js
+++ models/scholarship.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const ScholarshipSchema = new Schema({
   name: { type: String, required: true },
-  deadline: { type: Date, required: true },
-  funding: { type: Number, required: true },
+  deadline: { type: String, required: true },
+  funding: { type: String, required: true },
   contactInfo: { type: String, required: true},
   // ALL REQUIREMENTS WILL BE INDIVIDUALLY QUERIED FROM THE RAW TEXT
   requirements: { type: String, required: true}
 });
 
-module.exports = mongoose.model("Scholarship", ScholarshipSchema);
+module.exports = mongoose.model('Scholarship', ScholarshipSchema);
--- src/routes/api/requirement.js
+++ src/routes/api/requirement.js
@@ -11,4 +11,17 @@ router.get('/scholarships', (req, res) => {
     .then(scholarships => res.json(scholarships));
 });
 
+// LOOK FOR SCHOLARSHIPS THAT HAVE GPA 3.0 IN REQUIRMENTS FILED
+router.get("/scolarship/3.0", (req, res) => {
+  Scholarship.find({tags: new RegExp(req.params.value, 'REGEX')}, function(err, f){
+    if(err){
+      console.log(err); 
+    } else {
+      res.render("Images/index.ejs", {
+        playlists: f
+      }); 
+    }
+  });
+});
+
 module.exports = router;
