d0937827d4d888bb8586fa91b3c11da8f042915a
Thomas <thomaslee9474@gmail.com>
2019-02-11 19:01:19 -0800

    [update] requirements.js

--- src/routes/api/requirement.js
+++ src/routes/api/requirement.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-undef */
 // API TO FETCH SCHOLARSHIP REQUIREMENTS
 const express = require('express');
 const router = express.Router();
@@ -5,43 +7,42 @@ const MongoClient = require('mongodb').MongoClient
 const databaseName = 'ScholarBoardData1234'
 let database
 let scholarship_collection
-let MongoURI = "mongodb://localhost:27017"
+let MongoURI = 'mongodb://localhost:27017'
 
 
 const Student = require('../../../models/student');
 const Scholarship = require('../../../models/scholarship');
 
 const dummyStudent = {
-  name: "Medi Assumani",
-  race: "Balck/African American"
+  name: 'Medi Assumani',
+  race: 'Balck/African American'
 }
 
 
-MongoClient.connect(MongoURI, function(error, connected_database){
-  if(error) throw error
-  if(!error){
+MongoClient.connect(MongoURI, (error, connected_database) => {
+  if (error) throw error;
+  if (!error) {
     database = connected_database.db(databaseName)
-    scholarship_collection = database.collection('scholarships')
+    scholarship_collection = database.collection('scholarships'); 
   }
 })
 
 // GET ALL SCHOLARSHIPS
 router.get('/scholarships', (req, res) => {
-  console.log("hi");
   Scholarship.find()
     .then(scholarships => res.json(scholarships));
 });
 
 // LOOK FOR SCHOLARSHIPS THAT HAVE GPA 3.0 IN REQUIRMENTS FILED
-router.get("/scolarship/3.0", (req, res) => {
-  scholarship_collection.find().toArray(function(err, result){
-    result.forEach(function(scholarship){
+router.get('/scolarship/3.0', (req, res) => {
+  scholarship_collection.find().toArray((err, result) => {
+    result.forEach((scholarship) => {
       curr_req = scholarship.requirements
-      if(curr_req.includes("3.0")){
-        console.log("This scholarship has 3.0");
+      if (curr_req.includes('3.0')) {
+        console.log('This scholarship has 3.0');
       }
-    })
-  })
+    });
+  });
 });
 
 module.exports = router;
