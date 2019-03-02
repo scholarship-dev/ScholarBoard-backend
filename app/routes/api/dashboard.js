d1f4e1c2ceccbc4aa848efc73eb45aac486958a4
Thomas <thomaslee9474@gmail.com>
2019-03-01 16:47:17 -0800

    [update] dashboard route

--- app/routes/api/dashboard.js
+++ app/routes/api/dashboard.js
@@ -4,65 +4,73 @@ const router = express.Router();
 const User = require('../../models/user');
 const Scholarship = require('../../models/scholarship');
 
-let currentUser = {
-  gpa: '2.5',
-  ethnicity: 'Hispanic',
-  educationLevel: 'college',
-  grade: 'senior'
-}
-
-// { gpa: { $lte: currentUser.gpa } },
-// { ethnicity: currentUser.ethnicity },
-// { educationLevel: currentUser.educationLevel },
-// { grade: currentUser.grade},
-
 /* ENDPOINT TO GET:
     - SCHOLARSHIPS THAT MATCH THE STUDENT PROFILE
     - CURRENT USER DATA TO RENDER ON DASHBOARD
+    - TESTED
 */
 router.get('/dashboard', (req, res) => {
-  // const currentUser = req.user
-  // const currentUser = dummyUser
-  // { ethnicity: currentUser.ethnicity }, {educationLevel: currentUser.educationLevel}
+  const currentUser = req.user
   console.log(currentUser);
-  Scholarship.find({ gpa: { $lte: currentUser.gpa } }).then((scholarships) => {
+  Scholarship.find({ 
+    $or: 
+      [ 
+        // ALL PAIRS
+        { $and: 
+         [ { gpa: { $lte: currentUser.gpa } },
+           { ethnicity: currentUser.ethnicity } ] },
+        { $and: 
+          [ { gpa: { $lte: currentUser.gpa } },
+            { educationLevel: currentUser.educationLevel } ] },
+        { $and: 
+          [ { gpa: { $lte: currentUser.gpa } },
+            { grade: currentUser.grade} ] },
+        { $and: 
+          [ { ethnicity: currentUser.ethnicity },
+            { educationLevel: currentUser.educationLevel } ] },
+        { $and: 
+          [ { ethnicity: currentUser.ethnicity },
+            { grade: currentUser.grade} ] },
+        { $and: 
+          [ { educationLevel: currentUser.educationLevel },
+            { grade: currentUser.grade} ] },
+        // ALL TRIPLETS
+        { $and: 
+          [ { gpa: { $lte: currentUser.gpa } },
+            { ethnicity: currentUser.ethnicity },
+            { educationLevel: currentUser.educationLevel } ] },
+        { $and: 
+          [ { gpa: { $lte: currentUser.gpa } },
+            { ethnicity: currentUser.ethnicity },
+            { grade: currentUser.grade} ] },
+        { $and: 
+          [ { gpa: { $lte: currentUser.gpa } },
+            { educationLevel: currentUser.educationLevel },
+            { grade: currentUser.grade} ] },
+        { $and: 
+          [ { ethnicity: currentUser.ethnicity },
+            { educationLevel: currentUser.educationLevel },
+            { grade: currentUser.grade} ] },
+        // ALL FOUR
+        { $and: 
+          [ { gpa: { $lte: currentUser.gpa } },
+            { ethnicity: currentUser.ethnicity },
+            { educationLevel: currentUser.educationLevel },
+            { grade: currentUser.grade} ] },
+      ] 
+  }).then((scholarships) => {
     User.find({ email: currentUser.email })
       .then((user) => {
-        res.status(200).send({ scholarhips, user });
+        res.status(200).send({ scholarships, user });
       }).catch((error) => {
         res.status(400).send({ error });
+        console.log(error);
       });
   })
     .catch((error) => {
       res.status(400).send({ error });
+      console.log(error);
     });
 });
 
-
 module.exports = router;
-
-// $or: 
-//       [ 
-//         // ALL PAIRS
-//         { $and: 
-//          [ { gpa: { $lte: currentUser.gpa } },
-//            { ethnicity: currentUser.ethnicity } ] },
-//         { $and: 
-//           [ { gpa: { $lte: currentUser.gpa } },
-//             { educationLevel: currentUser.educationLevel } ] },
-//         { $and: 
-//           [ { gpa: { $lte: currentUser.gpa } },
-//             { grade: currentUser.grade} ] },
-//         { $and: 
-//           [ { ethnicity: currentUser.ethnicity },
-//             { educationLevel: currentUser.educationLevel } ] },
-//         { $and: 
-//           [ { ethnicity: currentUser.ethnicity },
-//             { grade: currentUser.grade} ] },
-//         { $and: 
-//           [ { educationLevel: currentUser.educationLevel },
-//             { grade: currentUser.grade} ] },
-//         // ALL TRIPLETS
-
-//         // ALL FOUR
-//       ] 
