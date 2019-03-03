5de74b133967f0b9f36d300e0d4af1132c5497f0
Thomas Lee <thomaslee9474@gmail.com>
2019-03-03 14:40:57 -0800

    [removed] api-v1

--- source/index.routes.js
+++ source/index.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
-const scholarshipRoutes = require('./api-v2/scholarship/scholarship.route');
-const userRoutes = require('./api-v2/user/user.route');
-const authRouter = require('./api-v2/auth/auth');
+const scholarshipRoutes = require('./api/scholarship/scholarship.route');
+const userRoutes = require('./api/user/user.route');
+const authRouter = require('./api/auth/auth');
 
 const router = express.Router();
 
