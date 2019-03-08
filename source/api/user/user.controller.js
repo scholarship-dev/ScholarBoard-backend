b780df7603f054d04cfe109b280bc5f177659899
Medi Assumani <mediassumani49@gmail.com>
2019-03-07 18:40:25 -0800

    [Fix] some stuff

--- source/api/user/user.controller.js
+++ source/api/user/user.controller.js
@@ -8,64 +8,65 @@ const User = require('../user/user.model');
 // RETURNS ALL MATCHING SCHOLARSHIPS ACCORDING TO USER DATA
 async function MatchScholarship(req, res) {
 
-  const currentUser = req.user;
-  const scholarships = await Scholarship.find({ 
-    $or:
-      [ 
-        // ALL PAIRS
-        { $and: 
-         [ { gpa: { $lte: currentUser.gpa } },
-           { ethnicity: currentUser.ethnicity } ] },
-        { $and: 
-          [ { gpa: { $lte: currentUser.gpa } },
-            { educationLevel: currentUser.educationLevel } ] },
-        { $and: 
-          [ { gpa: { $lte: currentUser.gpa } },
-            { grade: currentUser.grade } ] },
-        { $and: 
-          [ { ethnicity: currentUser.ethnicity },
-            { educationLevel: currentUser.educationLevel } ] },
-        { $and: 
-          [ { ethnicity: currentUser.ethnicity },
-            { grade: currentUser.grade } ] },
-        { $and: 
-          [ { educationLevel: currentUser.educationLevel },
-            { grade: currentUser.grade } ] },
-        // ALL TRIPLETS
-        { $and: 
-          [ { gpa: { $lte: currentUser.gpa } },
-            { ethnicity: currentUser.ethnicity },
-            { educationLevel: currentUser.educationLevel } ] },
-        { $and: 
-          [ { gpa: { $lte: currentUser.gpa } },
-            { ethnicity: currentUser.ethnicity },
-            { grade: currentUser.grade } ] },
-        { $and: 
-          [ { gpa: { $lte: currentUser.gpa } },
-            { educationLevel: currentUser.educationLevel },
-            { grade: currentUser.grade } ] },
-        { $and: 
-          [ { ethnicity: currentUser.ethnicity },
-            { educationLevel: currentUser.educationLevel },
-            { grade: currentUser.grade } ] },
-        // ALL FOUR
-        { $and: 
-          [ { gpa: { $lte: currentUser.gpa } },
-            { ethnicity: currentUser.ethnicity },
-            { educationLevel: currentUser.educationLevel },
-            { grade: currentUser.grade } ] },
-      ] 
-  })
+  // const currentUser = req.user;
+  // const scholarships = await Scholarship.find({ 
+  //   $or:
+  //     [ 
+  //       // ALL PAIRS
+  //       { $and: 
+  //        [ { gpa: { $lte: currentUser.gpa } },
+  //          { ethnicity: currentUser.ethnicity } ] },
+  //       { $and: 
+  //         [ { gpa: { $lte: currentUser.gpa } },
+  //           { educationLevel: currentUser.educationLevel } ] },
+  //       { $and: 
+  //         [ { gpa: { $lte: currentUser.gpa } },
+  //           { grade: currentUser.grade } ] },
+  //       { $and: 
+  //         [ { ethnicity: currentUser.ethnicity },
+  //           { educationLevel: currentUser.educationLevel } ] },
+  //       { $and: 
+  //         [ { ethnicity: currentUser.ethnicity },
+  //           { grade: currentUser.grade } ] },
+  //       { $and: 
+  //         [ { educationLevel: currentUser.educationLevel },
+  //           { grade: currentUser.grade } ] },
+  //       // ALL TRIPLETS
+  //       { $and: 
+  //         [ { gpa: { $lte: currentUser.gpa } },
+  //           { ethnicity: currentUser.ethnicity },
+  //           { educationLevel: currentUser.educationLevel } ] },
+  //       { $and: 
+  //         [ { gpa: { $lte: currentUser.gpa } },
+  //           { ethnicity: currentUser.ethnicity },
+  //           { grade: currentUser.grade } ] },
+  //       { $and: 
+  //         [ { gpa: { $lte: currentUser.gpa } },
+  //           { educationLevel: currentUser.educationLevel },
+  //           { grade: currentUser.grade } ] },
+  //       { $and: 
+  //         [ { ethnicity: currentUser.ethnicity },
+  //           { educationLevel: currentUser.educationLevel },
+  //           { grade: currentUser.grade } ] },
+  //       // ALL FOUR
+  //       { $and: 
+  //         [ { gpa: { $lte: currentUser.gpa } },
+  //           { ethnicity: currentUser.ethnicity },
+  //           { educationLevel: currentUser.educationLevel },
+  //           { grade: currentUser.grade } ] },
+  //     ] 
+  // })
 
 
-  const user = await User.findOne({ email: currentUser.email });
-  res.send({ scholarships, user });
+  // const user = await User.findOne({ email: currentUser.email });
+  // res.send({ scholarships, user });
+  res.send("hi")
 }
 
 // GET USER PROFILE
 async function GetProfile(req, res) {
   const { email } = req.params;
-  const user = User.findOne({ email });
+  const user = await User.findOne({ email });
   res.send(user);
 }
 
