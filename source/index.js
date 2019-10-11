52955592dd5d59b43e247e314d30253c20467597
William Bogans <william.bogans@williambogans.dev>
2019-10-10 19:54:41 -0700

    cleaning up some of the files

--- source/index.js
+++ source/index.js
@@ -1,30 +1,9 @@
 const express = require("express");
-const { buildSchema } = require("graphql");
 const graphqlHTTP = require("express-graphql");
 const app = express();
 
-const schema = buildSchema(`
-type Query {
-    hello:String
-}
-`);
-
-const root = {
-  hello: () => {
-    return "Hello World";
-  }
-};
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true
-  })
-);
-
 const PORT = process.env.PORT || 4001;
+
 app.listen(PORT, () => {
-  console.log(`Your server is listening on ${PORT}`);
+  console.log(`Now listening on port 4000`);
 });
